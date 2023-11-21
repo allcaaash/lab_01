b968b49fea2b1c0f5955de895e526540f5253aef
all_caaash <une066@gmail.com>
2023-11-21 18:32:10 +0500

    example19_03.java completed

--- src/lab01/example19_01.java
+++ src/lab01/example19_01.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class example19_01 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         String lastName = "";
--- src/lab01/example19_02.java
+++ src/lab01/example19_02.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class example19_02 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         String name = "";
